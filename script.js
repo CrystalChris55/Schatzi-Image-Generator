c857a827d9d3b1d97c96433d5078d7901d2bf0e5
Christopher LaCasse <lacasse249@gmail.com>
2020-05-19 17:44:34 -0400

    Added more schatzi photos.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function random(){
-    var randomNumber = Math.floor(Math.random() * 20) + 7;
+    var randomNumber = Math.floor(Math.random() * 30) + 3;
     var imgName = "schatzi_" + randomNumber + ".jpg";
     document.getElementById("imageid").src="SchatziImgs" + "/" + imgName ;
     }
