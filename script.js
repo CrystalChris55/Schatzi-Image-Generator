474001ca76da38d2e865199a74c6887530d63971
Christopher LaCasse <lacasse249@gmail.com>
2020-05-25 15:28:48 -0400

    Renaming. Added Javascript clock.

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ function random(){
     var imgName = "schatzi_" + randomNumber + ".jpg";
     document.querySelector("img").src="SchatziImgs" + "/" + imgName ;
     }
+
+
+
+var span = document.getElementById('date');
+
+var d = new Date();
+var date = d.getDate();
+var month = d.getMonth() + 1; 
+var year = d.getFullYear();
+span.textContent = date + "/" + month + "/" + year;
+
+var span = document.getElementById('time');
+function time() {
+    var d = new Date();
+    var s = d.getSeconds();
+    var m = d.getMinutes();
+    var h = d.getHours();
+    span.textContent = h + ":" + m + ":" + s;
+    }
+setInterval(time, 1000);
+
+
